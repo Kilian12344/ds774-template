5f37f8eb585045b39817a5fad50f24901a511090
Kilian <kilian.oconnell@unh.edu>
2022-03-22 19:51:46 -0400

    main

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def admin():
             if result:
                 records = get_records() #added get records and print records here and at the else statement
                 print(records)
-                return render_template('admin.html', message='Thank you for your submission')
+                return render_template('admin.html', message='Thank you for your submission', records = records)
             else:
                 records = get_records()
                 print(records)
